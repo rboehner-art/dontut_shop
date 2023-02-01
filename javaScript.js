e37d33e282a1f370302ccdc7ca35974dac1c31b4
rboehner-art <110072465+rboehner-art@users.noreply.github.com>
2023-01-31 20:34:41 -0700

    dontut_shop

--- javaScript.js
+++ javaScript.js
@@ -1,4 +1,4 @@
-function myFunction() {
-    const myWindow = window.open("", "", "width=300,height=300");
-    myWindow.opener.document.getElementById("demo").innerHTML = "HELLO!";
-  }
+function openChildWindow() {
+  var windowFeatures = "width=1000, height=600, left=" + (window.outerWidth / 2 - 200) + ", top=" + (window.outerHeight / 2 - 150);
+  window.open("Order_Form.html", "Child Window", windowFeatures);
+}
--- shopping_cart.php
+++ shopping_cart.php
@@ -1,26 +1,54 @@
-<form action="submit-form.php" method="post">
-  <div>
-    <label for="name">Name:</label>
-    <input type="text" id="name" name="name">
-  </div>
-  <div>
-    <label for="address">Address:</label>
-    <input type="text" id="address" name="address">
-  </div>
-  <div>
-    <label for="payment">Payment Method:</label>
-    <select id="payment" name="payment">
-      <option value="credit">Credit Card</option>
-      <option value="debit">Debit Card</option>
-      <option value="paypal">Paypal</option>
-    </select>
-  </div>
-  <div>
-    <input type="submit" value="Submit">
-  </div>
-</form>
+<?php
+session_start();
 
-<form action="/cart" method="post">
+// Check if the cart exists in the session, if not create it
+if (!isset($_SESSION['cart'])) {
+  $_SESSION['cart'] = array();
+}
+
+// Check if the form was submitted
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  // Check if the item is already in the cart
+  $found = false;
+  foreach ($_SESSION['cart'] as &$item) {
+    if ($item['name'] == $_POST['item']) {
+      $item['quantity']++;
+      $found = true;
+      break;
+    }
+  }
+
+  // Add the item to the cart if it's not already there
+  if (!$found) {
+    array_push($_SESSION['cart'], array(
+      'name' => $_POST['item'],
+      'quantity' => 1
+    ));
+  }
+}
+
+// Check if the checkout form was submitted
+if (isset($_POST['checkout'])) {
+  // Process the checkout
+  echo "Processing the checkout...<br>";
+  echo "Contents of the cart:<br>";
+  foreach ($_SESSION['cart'] as $item) {
+    echo $item['name'] . " x " . $item['quantity'] . "<br>";
+  }
+
+  // Clear the cart
+  $_SESSION['cart'] = array();
+}
+?>
+
+<!-- Display the "Add to Cart" form -->
+<form action="" method="post">
   <input type="hidden" name="item" value="item1">
   <button type="submit">Add to Cart</button>
+</form>
+
+<!-- Display the checkout form -->
+<form action="" method="post">
+  <input type="hidden" name="checkout" value="1">
+  <button type="submit">Checkout</button>
 </form>
